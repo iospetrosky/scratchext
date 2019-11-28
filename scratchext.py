d0dc5dab1798a257d726fba673f4843c9a8d7d97
iospetrosky <lorenzo.pedrotti@gmail.com>
2019-11-28 12:52:00 +0000

    replaced main app

--- scratchext.py
+++ scratchext.py
@@ -1,11 +1,19 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
+ 
+@app.route("/")
+def index():
+    return "<html><body><h1>Test site running under Flask</h1></body></html>"
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+@app.route("/testext")
+def textext():
+    return render_template("testext.js")
 
+@app.route("/testget")
+def testget(A,B):
+    return "{} + {}".format(A,B)
+
+#commented to run under pythonanywhere.com    
+#if __name__ == "__main__":
+#    app.run(host='0.0.0.0',debug=True) 
+#    #app.run(host='10.115.202.155',debug=True)  
