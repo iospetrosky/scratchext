c3f2eb238fde8702c0da66c9b4cdbdc26838947d
iospetrosky <lorenzo.pedrotti@gmail.com>
2020-03-18 18:01:26 +0000

    Some more edits

--- microbit/testserial.js
+++ microbit/testserial.js
@@ -1,9 +1,3 @@
-basic.forever(function () {
-    if (input.isGesture(Gesture.Shake)) {
-        basic.showIcon(IconNames.Butterfly)
-        serial.writeLine("Earthquake")
-    }
-})
 /**
  * Micro:bit code to test the serial line and store data on the computer
  */
@@ -15,9 +9,17 @@ basic.forever(function () {
         basic.showIcon(IconNames.Sad)
         serial.writeLine("Croce")
     }
-    serial.writeLine("Current light:" + input.lightLevel())
-    serial.writeLine("Current temp :" + input.temperature())
+    serial.writeLine("Current light: " + input.lightLevel())
+    serial.writeLine("Current temp : " + input.temperature() + " Celsius")
     basic.pause(1000)
     basic.showIcon(IconNames.No)
     basic.pause(1000)
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        serial.writeLine("Button A")
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        serial.writeLine("Button B")
+    }
 })
